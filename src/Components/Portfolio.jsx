8c4eef7245196163207f8485bee195aad41c9aec
Talmeez Fuaad <87268503+itstalmeez@users.noreply.github.com>
2024-01-13 00:38:49 -0800

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -30,28 +30,28 @@ const imageAltText = "desktop with books and laptop";
  * links section. Below is a sample, update to reflect links you'd like to highlight.
  */
 const projectList = [
+  // {
+  //   title: "10 Things To Know About Azure Static Web Apps 🎉",
+  //   description:
+  //     "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
+  //   url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+  // },
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Finetuning DeepSeek Coder",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
-  },
-  {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "In this repo I fine-tuned DeepSeek Coder.",
+    url: "https://github.com/itstalmeez/Finetuned-DeepSeek-Code",
   },
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Created from Github Pages",
+    url: "https://itstalmeez.me",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Open Source Contribution",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
+      "Contribution To NASA's project",
     url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
   },
 ];
